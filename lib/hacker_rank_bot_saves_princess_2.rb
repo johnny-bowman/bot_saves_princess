32c4d1b6803dbc6a17948d8bc72527baf60887d5
Johnny Bowman <johntbowman.production@gmail.com>
2022-12-05 12:56:07 -0700

    Feat: HR Solution 2 nextMove prints next move

--- lib/hacker_rank_bot_saves_princess_2.rb
+++ lib/hacker_rank_bot_saves_princess_2.rb
@@ -1,4 +1,17 @@
-require 'pry'
+def nextMove(n,r,c,grid)
+    princess_coordinates = get_princess_coordinates(grid)
+    if c != princess_coordinates[0]
+        print_horizontal_move(c, princess_coordinates[0])
+    elsif r != princess_coordinates[1]
+        print_vertical_move(r, princess_coordinates[1])
+    end
+end
+
+def get_princess_coordinates(grid)
+    princess_row = get_princess_row(grid)
+    princess_column = get_princess_column(princess_row, grid)
+    return princess_column, princess_row
+end
 
 def get_princess_row(grid)
     princess_row = grid.find_index { |row| row.include?("p") }
@@ -9,7 +22,6 @@ def get_princess_column(princess_row, grid)
 end
 
 def print_horizontal_move(player_column, princess_column)
-    # binding.pry
     if player_column > princess_column
         puts "LEFT"
     else
