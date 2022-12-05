139234331f9f5f6fe58b6d7301408966f3a20fa5
Johnny Bowman <johntbowman.production@gmail.com>
2022-12-05 12:46:26 -0700

    Test: HR Solution 2 prints next horizontal or vertical move

--- spec/hacker_rank_bot_saves_princess_2_spec.rb
+++ spec/hacker_rank_bot_saves_princess_2_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Hacker Rank Solution 2" do
     before :each do
         @grid_1 = ['---p-', 'm----', '-----', '-----', '-----']
         @grid_2 = ['---m-', '-----', '--p--', '-----', '-----']
-        @grid_3 = ['-----', '-----', '-----', '-m---', '-p---']
+        @grid_3 = ['-----', '-----', '-----', '-m---', '--p--']
         @grid_4 = ['-p-', '---', 'm--']
     end
 
@@ -19,7 +19,21 @@ RSpec.describe "Hacker Rank Solution 2" do
     it 'finds the princess column' do
         expect(get_princess_column(0, @grid_1)).to eq(3)
         expect(get_princess_column(2, @grid_2)).to eq(2)
-        expect(get_princess_column(4, @grid_3)).to eq(1)
+        expect(get_princess_column(4, @grid_3)).to eq(2)
         expect(get_princess_column(0, @grid_4)).to eq(1)
+    end
+
+    it 'prints horizontal move towards princess' do
+        expect { print_horizontal_move(0, 3) }.to output("RIGHT\n").to_stdout
+        expect { print_horizontal_move(3, 2) }.to output("LEFT\n").to_stdout
+        expect { print_horizontal_move(1, 2) }.to output("RIGHT\n").to_stdout
+        expect { print_horizontal_move(0, 1) }.to output("RIGHT\n").to_stdout
+    end
+
+    it 'prints vertical move towards princess' do
+        expect { print_vertical_move(1, 0) }.to output("UP\n").to_stdout
+        expect { print_vertical_move(0, 2) }.to output("DOWN\n").to_stdout
+        expect { print_vertical_move(3, 4) }.to output("DOWN\n").to_stdout
+        expect { print_vertical_move(2, 0) }.to output("UP\n").to_stdout
     end
 end
