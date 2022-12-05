aee05529c6e66a3dbc2d8808420a841083481b53
Johnny Bowman <johntbowman.production@gmail.com>
2022-12-05 12:46:40 -0700

    Feat: HR Solution 2 prints next horizontal or vertical move

--- lib/hacker_rank_bot_saves_princess_2.rb
+++ lib/hacker_rank_bot_saves_princess_2.rb
@@ -1,9 +1,28 @@
+require 'pry'
+
 def get_princess_row(grid)
     princess_row = grid.find_index { |row| row.include?("p") }
 end
 
 def get_princess_column(princess_row, grid)
     princess_column = grid[princess_row].index("p")
+end
+
+def print_horizontal_move(player_column, princess_column)
+    # binding.pry
+    if player_column > princess_column
+        puts "LEFT"
+    else
+        puts "RIGHT"
+    end
+end
+
+def print_vertical_move(player_row, princess_row)
+    if player_row > princess_row
+        puts "UP"
+    else
+        puts "DOWN"
+    end
 end
 
 # n = gets.to_i
