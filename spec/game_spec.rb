51398565ea5193ddf3f96640d55ccb2cdb443460
Johnny Bowman <johntbowman.production@gmail.com>
2022-12-05 13:07:22 -0700

    Test: Game class attributes and methods

---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require './lib/game'
+
+RSpec.describe "Game" do
+    before :each do
+        @game = Game.new(5) 
+    end
+
+    it 'exists' do
+        expect(@game).to be_an_instance_of(Game)
+    end
+
+    it 'has readable size attribute' do
+        expect(@game.size).to eq(5)
+    end
+
+    it 'has readable grid attribute' do
+        expect(@game.grid).to eq(['-----', '-----', '-----', '-----', '-----',])
+    end
+
+    it 'prints grid to terminal' do
+        expect { @game.print_grid }.to output("-----\n" * 5).to_stdout
+    end
+end
